53d51a293730a3fc3e3fd447d883debbd8d4ba40
jeffreyroshan2006-crypto <jeffreyroshan2006@gmail.com>
2026-02-06 22:03:22 +0530

    fix: Simplify build script to use readFile instead of JSON import assertion

--- script/build.mjs
+++ script/build.mjs
@@ -1,6 +1,6 @@
-// Build script - Uses esbuild from devDependencies
+// Build script - Simplified for Vercel
 import { build as viteBuild } from "vite";
-import { rm } from "fs/promises";
+import { rm, readFile } from "fs/promises";
 import { build as esbuild } from "esbuild";
 
 // server deps to bundle
@@ -28,10 +28,13 @@ async function buildAll() {
 
   console.log("building server...");
   
-  const pkg = await import("../package.json", { assert: { type: "json" } });
+  // Read package.json using readFile
+  const pkgContent = await readFile("package.json", "utf-8");
+  const pkg = JSON.parse(pkgContent);
+  
   const allDeps = [
-    ...Object.keys(pkg.default.dependencies || {}),
-    ...Object.keys(pkg.default.devDependencies || {}),
+    ...Object.keys(pkg.dependencies || {}),
+    ...Object.keys(pkg.devDependencies || {}),
   ];
   const externals = allDeps.filter((dep) => !allowlist.includes(dep));
 
@@ -50,6 +53,6 @@ async function buildAll() {
 }
 
 buildAll().catch((err) => {
-  console.error(err);
+  console.error("Build failed:", err);
   process.exit(1);
 });
